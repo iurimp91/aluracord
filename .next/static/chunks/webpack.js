9f577642ddc21d3857b1a129889d09be9424325a
Iuri Magnago <iurimp91@gmail.com>
2022-01-24 18:24:19 -0300

    feat: add homepage layout and styles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "762093fdbf8fd0ea"; }
+/******/ 		__webpack_require__.h = function() { return "2705a5d7cf0d1f39"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
