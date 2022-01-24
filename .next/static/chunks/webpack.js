c25b3e2226a93a9605ade7892352606443105ff3
Iuri Magnago <iurimp91@gmail.com>
2022-01-24 17:31:01 -0300

    chore: work with tag in component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "053fcc241c7c8c89"; }
+/******/ 		__webpack_require__.h = function() { return "6cfb5a892939a229"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 function Title(props) {
+  const Tag = props.tag;
+  
   return (
     <>
-      <h1>{props.children}</h1>
+      <Tag>{props.children}</Tag>
       <style jsx>{`
-      h1 {
+      ${Tag}1 {
         color: red;
+        font-size: 24px;
+        font-weight: 600;
       }
       `}</style>
     </>
@@ -14,7 +18,7 @@ function Title(props) {
 function HomePage() {
   return ( 
     <div>
-      <Title>Boas vindas de volta!</Title>
+      <Title tag="h2">Boas vindas de volta!</Title>
       <h2>Discord - Alura Matrix</h2>
     </div>
   );
