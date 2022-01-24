9c8ca6625d2bb1658c61b68c9ef54553b86f7c48
Iuri Magnago <iurimp91@gmail.com>
2022-01-24 17:12:13 -0300

    chore: create first  component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5e18659c3c2bb739"; }
+/******/ 		__webpack_require__.h = function() { return "053fcc241c7c8c89"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
-function HomePage() {
-  return ( 
-    <div>
-      <h1>Boas vindas de volta!</h1>
-      <h2>Discord - Alura Matrix</h2>
+function Title(props) {
+  return (
+    <>
+      <h1>{props.children}</h1>
       <style jsx>{`
       h1 {
         color: red;
       }
       `}</style>
+    </>
+  );
+}
+
+function HomePage() {
+  return ( 
+    <div>
+      <Title>Boas vindas de volta!</Title>
+      <h2>Discord - Alura Matrix</h2>
     </div>
   );
 }
